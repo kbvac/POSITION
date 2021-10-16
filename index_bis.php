ae29da27f492e2a565125aa51e957c8db8f610b0
kbvac <angechristian2001.ac@gmail.com>
2021-10-16 23:32:31 +0000

    ajout homePage scrollable

---
+++ index_bis.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="stylebis.css">
+      <script src="https://code.iconify.design/2/2.0.3/iconify.min.js"></script>
+      <title>POSITION</title>
+</head>
+<body>
+      <header>
+            <a href="#" id="logo">LOGO</a>
+            <ul>
+                  <li><a href="#" class="actif">ACCUEIL</a></li>
+                  <li><a href="#">EVENT</a></li>
+                  <li><a href="#">ABOUT</a></li>
+            </ul>
+            <a href="#" id="sub">S'INSCRIRE</a>
+      </header>
+      <section>
+            <img src="./img/bgHeader.png" alt="">
+            
+            <div id="grid-container">
+            <div id="titre">LES PLUS TENDANCES</div>
+            <div class="post">
+            <div class="titre-container">
+                    <div class="profile-picture"></div>
+                    <div class="profile-name">photo name</div>
+                </div>
+                <div class="img"></div>
+                <div class="btnform"><a id="btn" href="">rejoindre</a></div>
+            </div>
+
+            <div class="post">
+            <div class="titre-container">
+                    <div class="profile-picture"></div>
+                    <div class="profile-name">photo name</div>
+                </div>
+                <div class="img"></div>
+                <div class="btnform"><a href="" id="btn">rejoindre</a></div>
+            </div>
+
+            <div class="post">
+            <div class="titre-container">
+                    <div class="profile-picture"></div>
+                    <div class="profile-name">photo name</div>
+                </div>
+                <div class="img"></div>
+                <div class="btnform"><a id="btn" href="">rejoindre</a></div>
+            </div>
+            </div>
+            <span class="iconify" href="" id="arrow" data-icon="dashicons:arrow-up-alt2" style="color: white;" data-width="70" data-height="70" data-rotate="180deg"></span>
+      </section>
+      <section id="discover">
+            <span id="product-title">NOS OFFRES</span>
+            <img src="./img/Groupe 13.png" id="product-img" alt="">
+            <div id="offers-container">
+
+                  <div id="offer">
+                        <span id="offer-title">SERVICE <span>ADHERENT</span></span>
+                        <ul>
+                              <li>Participation a des events</li>
+                              <li class="no">Organiser des events</li>
+                              <li class="no">Percevoir des gains</li>
+                              <li class="no">Systèmes 60/40</li>
+                              <li>Promotion Diverse</li>
+                        </ul>
+                        <div id="price">GRATUIT</div>
+                  </div>
+                  <div id="offer">
+                        <span id="offer-title">SERVICE <span>ORGANISATEUR</span></span>
+                        <ul>
+                              <li class="no">Participation a des events</li>
+                              <li>Organiser des events</li>
+                              <li>Percevoir des gains</li>
+                              <li>Systèmes 60/40</li>
+                              <li>Promotion Diverse(Payant)</li>
+                        </ul>
+                        <div id="price">GRATUIT</div>
+                  </div>
+                  <div id="offer">
+                        <span id="offer-title">SERVICE <span>PREMIUM</span></span>
+                        <ul>
+                              <li>Participation a des events</li>
+                              <li>Organiser des events</li>
+                              <li>Percevoir des gains</li>
+                              <li>Systèmes 70/30</li>
+                              <li>Promotion Diverse</li>
+                        </ul>
+                        <div id="price">OBTENIR</div>
+                  </div>
+
+            </div>
+            <span id="details">PLUS</span>
+      </section>
+      <section id="news"></section>
+      <footer id="footer"></footer>
+</body>
+</html>
